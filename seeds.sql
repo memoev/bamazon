026905f6c712688b2b86e37380c9212729c5ca69
Guillermo Villalta <mexcelus@gmail.com>
2019-10-05 15:58:43 -0600

    schema and seeds added

---
+++ seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO parties (product_name, department_name, price, stock_quantity) VALUES ('Guitar', 'Music', 100, 30);
+INSERT INTO parties (product_name, department_name, price, stock_quantity) VALUES ('Drums', 'Music', 200, 20);
+INSERT INTO parties (product_name, department_name, price, stock_quantity) VALUES ('Watch', 'Accessories', 80, 35);
+INSERT INTO parties (product_name, department_name, price, stock_quantity) VALUES ('Keychain', 'Accessories', 2.50, 100);
+INSERT INTO parties (product_name, department_name, price, stock_quantity) VALUES ('Mortal Kombat', 'Games', 49.99, 80);
+INSERT INTO parties (product_name, department_name, price, stock_quantity) VALUES ('Call Of Duty', 'Games', 49.99, 70);
+INSERT INTO parties (product_name, department_name, price, stock_quantity) VALUES ('Laptop', 'Technology', 1000, 80);
